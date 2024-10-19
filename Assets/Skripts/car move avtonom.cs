573f62810859e6074e4a18aa6fe9175b0ac21167
Telekrafter <artem.frolov2103@gmail.com>
2024-10-19 17:13:08 +0300

    Initial commit

---
+++ Assets/Skripts/car move avtonom.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carmoveavtonom : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(70, 37, -48);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
